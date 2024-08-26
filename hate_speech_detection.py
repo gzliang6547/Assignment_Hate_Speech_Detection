f6df02057c0a71deed766426df18021fcf6f1298
gzliang6547 <141334045+gzliang6547@users.noreply.github.com>
2024-08-26 20:54:56 +0800

    Update hate_speech_detection.py

--- hate_speech_detection.py
+++ hate_speech_detection.py
@@ -16,6 +16,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 
+nltk.download('stopwords')
 
 # Load the TF-IDF vectorizer and all hate speech detection model
 tfidf_loaded = load('tfidf_vectorizer.joblib')
