f07155eadaa2872b323f4fdd7be11f4135e492d1
gzliang6547 <141334045+gzliang6547@users.noreply.github.com>
2024-08-26 21:21:21 +0800

    Update hate_speech_detection.py

--- hate_speech_detection.py
+++ hate_speech_detection.py
@@ -4,7 +4,6 @@ import string
 import contractions
 import streamlit as st
 import nltk
-nltk.download('stopwords')
 from nltk.corpus import stopwords
 from nltk.stem import LancasterStemmer
 from nltk.stem import WordNetLemmatizer
@@ -17,6 +16,10 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
+
+nltk.download('stopwords')
+nltk.download("wordnet")
+nltk.download("omw-1.4")
 
 # Load the TF-IDF vectorizer and all hate speech detection model
 tfidf_loaded = load('tfidf_vectorizer.joblib')
