d6b435334ee279ac0163e7b12739bc6f0b5e5072
gzliang6547 <141334045+gzliang6547@users.noreply.github.com>
2024-09-07 17:39:45 +0800

    Update hate_speech_detection.py

--- hate_speech_detection.py
+++ hate_speech_detection.py
@@ -6,8 +6,8 @@ import streamlit as st
 import nltk
 nltk.download('stopwords')
 nltk.download('punkt_tab')
-nltk.download('punkt')
 nltk.download('wordnet')
+nltk.download('omw-1.4')
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer,LancasterStemmer
 from nltk.classify import apply_features
