8dec97149f541dc59bfb44537474d26b1c018dbc
gzliang6547 <141334045+gzliang6547@users.noreply.github.com>
2024-09-07 17:40:55 +0800

    Update hate_speech_detection.py

--- hate_speech_detection.py
+++ hate_speech_detection.py
@@ -153,7 +153,7 @@ def preprocess_and_clean(sentences):
     # create lemmatizer object
     lemmatizer = WordNetLemmatizer()
     # lemmatize each word
-    sentences_df['Sentences'] = sentences_df['Sentences'].apply(lambda x: ' '.join([lemmatizer.lemmatize(word) for word in x.split()]))
+    sentences_df['Sentences'] = sentences_df['Sentences'].apply(lambda x: ' '.join([word.lemmatize for word in TextBlob(x).words]))
     
     # create stemming object
     stemmer = LancasterStemmer()
