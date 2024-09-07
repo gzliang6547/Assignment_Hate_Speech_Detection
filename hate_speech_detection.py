9d1277e0e5bd9c373031709b0f4cdde3e85a7bad
gzliang6547 <141334045+gzliang6547@users.noreply.github.com>
2024-09-07 15:25:27 +0800

    Update hate_speech_detection.py

--- hate_speech_detection.py
+++ hate_speech_detection.py
@@ -49,7 +49,7 @@ def main():
         if st.button('Predict'):
             if user_input:  # Check if the input is not empty
                 processed_user_input = preprocess_and_clean([user_input]) # Preprocess text
-                if processed_user_input is None:
+                if processed_user_input is not None:
                     predict_and_display([user_input],processed_user_input)  # Single sentence prediction
                 else:
                     st.error("Please enter another sentence for prediction.")
