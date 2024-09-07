51373e7e1f6dad64debe57f10fb680aeaaa2e1f8
gzliang6547 <141334045+gzliang6547@users.noreply.github.com>
2024-09-07 17:25:06 +0800

    Update hate_speech_detection.py

--- hate_speech_detection.py
+++ hate_speech_detection.py
@@ -45,8 +45,9 @@ def main():
     # Option to manually enter text
     if option == "Manually Enter Text":       
         # Text box for user input
-        st.subheader(":orange[Enter a sentence to check it's hate speech score and determine if it's hate speech or not]\n(Higher Hate Speech Score = More Hateful)")    
-        user_input = st.text_input("Your Sentence:Word("kidssss").lemmatize()")
+        st.subheader(":orange[Enter a sentence to check it's hate speech score and determine if it's hate speech or not]\n(Higher Hate Speech Score = More Hateful)") 
+        st.markdown(Word("kidssss").lemmatize())
+        user_input = st.text_input("Your Sentence:")
 
         # Predict button
         if st.button('Predict'):
